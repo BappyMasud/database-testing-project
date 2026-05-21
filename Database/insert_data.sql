fa82728c1de5843c06c97b80d110f01f40ec3940
Bappy Masud <bappymasud9@gmail.com>
2026-05-22 00:18:36 +0600

    Initial SQL database project

--- Database/insert_data.sql
+++ Database/insert_data.sql
@@ -5,3 +5,27 @@ VALUES
 ('Akash Nahid', 'akash@gmail.com', '01933333333', 'Satkhira'),
 ('Himel Ahmmed', 'himel@gmail.com', '01344444444', 'Barishal'),
 ('Nargis Aktar', 'nargis@gmail.com', '01655555555', 'Jamalpur');
+
+INSERT INTO products(product_name, category, price, stock_quantity)
+VALUES
+('Laptop', 'Electronics', 85000, 20),
+('Mouse', 'Electronics', 1200, 100),
+('Keyboard', 'Electronics', 2500, 50),
+('Monitor', 'Electronics', 15000, 15),
+('Mobile', 'Electronics', 50000, 2);
+
+INSERT INTO orders(customer_id, order_date, total_amount, order_status)
+VALUES
+(1, '2026-05-01', 86200, 'Completed'),
+(2, '2026-05-02', 17500, 'Pending');
+
+INSERT INTO payments(order_id, payment_method, payment_status, payment_date)
+VALUES
+(1, 'Card', 'Paid', '2026-05-01'),
+(2, 'Card', 'Pending', '2026-05-02');
+
+INSERT INTO order_details(order_id, product_id, quantity, subtotal)
+VALUES
+(1, 1, 1, 85000),
+(1, 2, 1, 1200),
+(2, 4, 1, 15000);
