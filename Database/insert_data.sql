2fa025fe7fd6b4dc54c248f46ddf72c70e3995fb
Bappy Masud <bappymasud9@gmail.com>
2026-05-21 18:14:27 +0600

    added customers details file

---
+++ Database/insert_data.sql
@@ -0,0 +1,7 @@
+INSERT INTO customers(full_name, email, phone, city)
+VALUES
+('Sohel Masud', 'sohel@gmail.com', '01711111111', 'Dahak'),
+('Niloy Debnath', 'niloy@gmail.com', '01822222222', 'Chittagong'),
+('Akash Nahid', 'akash@gmail.com', '01933333333', 'Satkhira'),
+('Himel Ahmmed', 'himel@gmail.com', '01344444444', 'Barishal'),
+('Nargis Aktar', 'nargis@gmail.com', '01655555555', 'Jamalpur');
