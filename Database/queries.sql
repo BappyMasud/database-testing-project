7b6ae552705316f9c1e391dc680ecb52d84cc59e
Bappy Masud <bappymasud9@gmail.com>
2026-05-26 20:35:36 +0600

    Added database-queries file

---
+++ Database/queries.sql
@@ -0,0 +1,14 @@
+-- View all employee records
+SELECT * 
+FROM employees;
+
+-- View employees with salary greater than 50000
+SELECT *
+FROM employees
+WHERE salary > 50000;
+
+-- View employees from IT department
+SELECT *
+FROM employees
+WHERE department = 'IT';
+
