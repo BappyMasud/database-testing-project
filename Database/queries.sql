16397dbfd8d8e0c0cf927282f5a92c5d0e1176b5
Bappy Masud <bappymasud9@gmail.com>
2026-06-04 20:26:18 +0600

    Updated queries file

--- Database/queries.sql
+++ Database/queries.sql
@@ -25,3 +25,30 @@ ORDER BY order_date ASC;
 SELECT DISTINCT email
 FROM customers;
 
+-- Count total customer
+SELECT COUNT(*) AS total_customer
+FROM customers;
+
+-- Find highest purchase amount
+SELECT MAX(total_amount) AS highest_amount
+FROM orders;
+
+-- Find lowest purchase amount
+SELECT MIN(total_amount) AS lowest_amount
+FROM orders;
+
+-- Find average sell
+SELECT AVG(total_amount) AS average_sell
+FROM orders;
+
+-- Find total sell
+SELECT SUM(total_amount) AS total_sell
+FROM orders;
+
+-- Search customer's whose first name start with 'A'
+SELECT * FROM customers
+WHERE full_name LIKE 'A%';
+
+-- Search customer's whose last name end with 'r'
+SELECT * FROM customers
+WHERE full_name LIKE '%r'
